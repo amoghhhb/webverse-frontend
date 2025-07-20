139a3b2d808fd62b3343bca92fd6f9165ca029e6
amoghhhb <5070875@bkbck.edu.in>
2025-07-21 02:22:52 +0530

    Fixed prop-types and build issues

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  },
-  server: {
-    port: 3000
+    rollupOptions: {
+      external: ['prop-types']
+    }
   }
 })
